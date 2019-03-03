cf93ebe71d91bf356767ac564da0aae1fd652394
Christopher Liu <cdl7an@virginia.edu>
2019-03-02 22:23:45 -0500

    added visibility changes to homepage

--- app/src/main/java/com/example/dingliu/creatorcodetfue/MainActivity.java
+++ app/src/main/java/com/example/dingliu/creatorcodetfue/MainActivity.java
@@ -7,16 +7,24 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private SearchView searchView;
+    private ListView listView;
+    private TextView basketText;
+    private RecyclerView basketList;
+    private TextView recipeText;
+    private RecyclerView recipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.menu);
+
+        // set search list and make it gone
+        listView = findViewById(R.id.search_list);
+        listView.setVisibility(View.GONE);
+
+        basketText = findViewById(R.id.basket_text);
+        basketList = findViewById(R.id.basket_list);
+        recipeText = findViewById(R.id.recipe_text);
+        recipeList = findViewById(R.id.recipe_list);
 
 
 
@@ -52,13 +69,28 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                Toast.makeText(getApplicationContext(), query + "!!!!!", Toast.LENGTH_SHORT).show();
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                if(newText.equals("")){
+                    listView.setVisibility(View.GONE);
+                    basketText.setVisibility(View.VISIBLE);
+                    basketList.setVisibility(View.VISIBLE);
+                    recipeText.setVisibility(View.VISIBLE);
+                    recipeList.setVisibility(View.VISIBLE);
+                }
+                else {
+                    listView.setVisibility(View.VISIBLE);
+                    basketText.setVisibility(View.GONE);
+                    basketList.setVisibility(View.GONE);
+                    recipeText.setVisibility(View.GONE);
+                    recipeList.setVisibility(View.GONE);
+                }
                 Toast.makeText(getApplicationContext(), newText, Toast.LENGTH_SHORT).show();
+
+
                 return true;
             }
         });
@@ -85,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
+
     }
 
     @Override
@@ -96,5 +129,8 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
 
 }
