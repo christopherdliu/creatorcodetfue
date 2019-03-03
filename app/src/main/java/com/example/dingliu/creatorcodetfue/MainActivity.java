23ad453a237cb771e52a48228ec036630e34cdd3
christopherdliu <35649146+christopherdliu@users.noreply.github.com>
2019-03-02 19:02:00 -0500

    added menu button to the top action bar

--- app/src/main/java/com/example/dingliu/creatorcodetfue/MainActivity.java
+++ app/src/main/java/com/example/dingliu/creatorcodetfue/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,7 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.menu);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         searchView = findViewById(R.id.searchView);
         searchView.setQueryHint("Search");
@@ -35,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
                 searchView.setIconified(false);
             }
         });
-
         searchView.onActionViewExpanded();
         new Handler().postDelayed(new Runnable()
         {
@@ -45,5 +50,34 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 300);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
