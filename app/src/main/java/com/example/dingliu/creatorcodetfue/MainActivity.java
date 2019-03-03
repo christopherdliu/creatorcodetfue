e165b7b219ddb54c4767f9ed8e9b815327b75a6e
Christopher Liu <cdl7an@virginia.edu>
2019-03-02 21:38:40 -0500

    update

--- app/src/main/java/com/example/dingliu/creatorcodetfue/MainActivity.java
+++ app/src/main/java/com/example/dingliu/creatorcodetfue/MainActivity.java
@@ -2,16 +2,13 @@ package com.example.dingliu.creatorcodetfue;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.Toast;
